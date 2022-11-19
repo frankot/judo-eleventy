1eeeae8b671d7a1c694f7c1ddad50b2a4c91ce99
frank <frankiantki@gmail.com>
2022-11-19 20:10:06 +0100

    before camp desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,24 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'hero':['Lazer84'],
+        'hero': ['Lazer84'],
+        'inter-regular': ['Inter-Regular'],
+        'inter-light': ['Inter-Light'],
+        'inter-extraBold': ['Inter-ExtraBold'],
+        'inter-bold': ['Inter-Bold']
       },
       colors: {
         'cust-black': '#101011',
         'cust-yellow': '#ffd300',
         'cust-yellow-hover': '#e5bd00',
+        'cust-white': '#F2F3F3',
+        'cust-green': '#81D449',
+        'cust-blu': '#3B7CF3',
+        'cust-red': '#EB4511',
+      },
+      screens: {
+        'md': '915px',
+        'camp': '800px'
       }
     },
   },
